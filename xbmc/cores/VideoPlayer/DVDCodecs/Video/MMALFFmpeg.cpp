has12/xbmc

rbp: Support zero copy interface with hevc acceleration

xbmc/cores/VideoPlayer/DVDCodecs/Video/MMALFFmpeg.cpp
@@ -281,8 +281,9 @@ bool CDecoder::GetPicture(AVCodecContext* avctx, AVFrame* frame, DVDVideoPicture
   picture->MMALBuffer->mmal_buffer->data = (uint8_t *)gmem->m_vc_handle;
   picture->MMALBuffer->mmal_buffer->alloc_size = picture->MMALBuffer->mmal_buffer->length = gmem->m_numbytes;
 
-  // need to flush ARM cache so GPU can see it
-  gmem->Flush();
+  // need to flush ARM cache so GPU can see it (HEVC will have already done this)
+  if (avctx->codec_id != AV_CODEC_ID_HEVC)
+    gmem->Flush();
 
   if (g_advancedSettings.CanLogComponent(LOGVIDEO))
     CLog::Log(LOGDEBUG, "%s::%s - mmal:%p dts:%.3f pts:%.3f buf:%p gpu:%p", CLASSNAME, __FUNCTION__, picture->MMALBuffer->mmal_buffer, 1e-6*picture->dts, 1e-6*picture->pts, picture->MMALBuffer, gmem);

rofehr/xbmc-1

xbmc/cores/VideoPlayer/DVDCodecs/Video/MMALFFmpeg.cpp
@@ -298,8 +298,9 @@ CDVDVideoCodec::VCReturn CDecoder::Decode(AVCodecContext* avctx, AVFrame* frame)
 
     CGPUMEM *m_gmem = (CGPUMEM *)av_buffer_get_opaque(frame->buf[0]);
     assert(m_gmem);
-    // need to flush ARM cache so GPU can see it
-    m_gmem->Flush();
+    // need to flush ARM cache so GPU can see it (HEVC will have already done this)
+    if (avctx->codec_id != AV_CODEC_ID_HEVC)
+      m_gmem->Flush();
     m_renderBuffer = static_cast<CMMALYUVBuffer*>(m_gmem->m_opaque);
     assert(m_renderBuffer && m_renderBuffer->mmal_buffer);
     if (m_renderBuffer)